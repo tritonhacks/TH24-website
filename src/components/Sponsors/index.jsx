c02315ae0d47259cb07a1379ea7409d260fd4e10
Kailey Wong <kaileyswong@gmail.com>
2023-04-28 17:15:43 -0700

    added sponsors

--- src/components/Sponsors/index.jsx
+++ src/components/Sponsors/index.jsx
@@ -2,7 +2,17 @@ import "./style.css";
 import smoke from "../../assets/smoke-bottom.svg";
 import alien from "../../assets/alien.svg";
 import beam from "../../assets/beam.svg";
-import cseucsd from "../../assets/cseucsd.png";
+import cseucsd from "../../assets/logos/cseucsd.png";
+import codeorg from "../../assets/logos/codeorg.svg";
+import create from "../../assets/logos/create.svg";
+import csfe from "../../assets/logos/csfe-logo.svg";
+import cstasd from "../../assets/logos/csta.svg";
+import familia from "../../assets/logos/familia.svg";
+import googlecloud from "../../assets/logos/google-cloud.png";
+import netapp from "../../assets/logos/netapp.svg";
+import trace3 from "../../assets/logos/trace3.svg";
+import ucsdidea from "../../assets/logos/ucsd-idea.svg";
+
 
 const Sponsors = () => {
   return (
@@ -24,8 +34,31 @@ const Sponsors = () => {
         .
       </p>
       <div id="sponsor-images">
+        <a href="https://csforeach.ucsd.edu/" target="_blank" rel="noreferrer">
+          <img src={csfe} alt="CSforeach"></img>
+        </a>
         <a href="https://cse.ucsd.edu/" target="_blank" rel="noreferrer">
           <img src={cseucsd} alt="UCSD CSE"></img>
+        </a>
+        <a href="https://cloud.google.com/" target="_blank" rel="noreferrer">
+          <img src={googlecloud} alt="Google Cloud"></img>
+        </a>
+        <a href="https://sandiego.csteachers.org/page/about-us" target="_blank" rel="noreferrer">
+          <img src={cstasd} alt="CSTA San Diego"></img>
+        </a>
+        <a href="https://www.netapp.com/" target="_blank" rel="noreferrer">
+          <img src={netapp} alt="NetApp"></img>
+        </a>
+        <a href="https://code.org/" target="_blank" rel="noreferrer">
+          <img src={codeorg} alt="Code.org"></img>
+        </a>
+        <a href="https://create.ucsd.edu/" target="_blank" rel="noreferrer">
+          <img src={create} alt="UCSD Create"></img>
+        </a>
+          <img src={familia} alt="UCSD CSE"></img>
+          <img src={trace3} alt="UCSD CSE"></img>
+        <a href="https://jacobsschool.ucsd.edu/idea" target="_blank" rel="noreferrer">
+          <img src={ucsdidea} alt="UCSD Idea Center"></img>
         </a>
       </div>
 
